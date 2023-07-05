b1dc059dc3c7b927d3f68b832bd5d87696b94721
EnricoValente <enricovalente42@gmail.com>
2023-07-05 18:03:52 +0200

    richiesti dati usando libreria axios

---
+++ js/script.js
@@ -0,0 +1,43 @@
+
+
+
+
+const {createApp}= Vue;
+
+createApp({
+    data(){
+        return{
+
+            emails: [
+
+            ],
+
+        }
+    },
+    methods: {
+
+    },
+    created(){
+
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                const result= response.data;
+                this.emails.push({
+                   text:response.data.response 
+                })
+                
+            })
+            
+        
+            
+        }
+
+    }
+}).mount('#app');
+
+
+
+
+
+    
